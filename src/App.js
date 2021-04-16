71ec4451b386fdf08812c50ee1a1a412a15c6edd
Agustina Claro Hanel <agustinaclaro@hotmail.com>
2021-04-16 09:52:16 -0300

    logo-navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 
 import './App.css';
 import {Navbar} from './components/navbar/navbar'
-import {logo} from '../img logo.png';
-console.log(logo)
 
 function App() {
   return (
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,4 +1,4 @@
-
+import {logo} from '../../logo.svg'
 
 export const Navbar = () => {
     return (
