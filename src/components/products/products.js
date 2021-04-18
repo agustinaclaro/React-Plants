eb4bc713fa01443ec11208d1a704fb606f44727f
Agustina Claro Hanel <agustinaclaro@hotmail.com>
2021-04-18 14:53:00 -0300

    after class img

--- src/components/products/products.js
+++ src/components/products/products.js
@@ -1,12 +1,12 @@
 import './products.scss'
 
 export const Products = (props)=> (
-    const productClick = ()=> (
-        alert( `compra ${props.title} que es tu planta ideal` ) )
+    const productClick = ()=> 
+        alert( `compra ${props.title} que es tu planta ideal` ) 
     return(
     <div onClick ={ productClick}>
         <h2>{props.title}</h2>
         <p>{props.price}</p>
-    </div>
+    </div> 
 )
 )
